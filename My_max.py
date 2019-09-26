91d050cc6d8b6071ee47fa72015445f4195d0fbe
Aranatell <Daniel.Burzunov@mail.ru>
2019-09-26 13:22:41 +0300

    init commit

--- My_max.py
+++ My_max.py
@@ -1,9 +1,9 @@
 #my_main_'max'_function
 def mymax(a,b):
     if (a-b)>=0:
-        print(a)
+        return(a)
     else:
-        print(b)
+        return(b)
 a=int(input())
 b=int(input())
 print(max(a,b))
